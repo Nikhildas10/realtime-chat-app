c7190d4ed6e5d689556efd1c822b5e73dd079221
Nikhildas10 <nikhildas1027@gmail.com>
2025-04-11 01:13:50 +0530

    bug:change in port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,15 +6,17 @@ import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 8000;
-  app.enableCors({ origins: '*' });
+  const port = parseInt(process.env.PORT || '8000', 10);
+
+  app.enableCors({ origin: '*' });
   app.use(morgan('dev'));
-  app.use(express.json({ limit: '50mb' })); 
+  app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(port);
+
+  await app.listen(port, '0.0.0.0'); 
   console.log(`🚀 Server is running on http://localhost:${port}`);
 }
 bootstrap();
